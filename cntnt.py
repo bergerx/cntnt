73e5afc19efb76c682bbc01f9718fc51dc5913c4
bekir <bekir@b67a7fcb-dc60-493a-b9c6-6ad9acd740d3>
2007-09-08 12:30:34 +0000

    * Added some TODOs, * Execptions on errors * read() funtion can follow pointers * some changes in CLI code portions * commit() moved into create() function from read()
    git-svn-id: https://svn.bdgn.net/public/test/python@12 b67a7fcb-dc60-493a-b9c6-6ad9acd740d3

--- cntnt.py
+++ cntnt.py
@@ -3,6 +3,18 @@ import sqlite
 import sys, re, datetime
 
 class cntnt:
+	# TODO: There are lots of confusion between id and contentid.
+	# id means version number, content id means id
+
+	# Exception Definitions
+	class LabelNameError(Exception): pass
+	class TypeNameError(Exception): pass
+	class ContentNotExixtsError(Exception): pass
+	class ParentNotFoundError(Exception): pass
+	class LabelNotUniqError(Exception): pass
+	class ContentHasChilds(Exception): pass
+	class ContentHasNoChilds(Exception): pass
+
 	# TODO: Add parameter check
 	def __init__(self, dbfile):
 		self.conn = sqlite.connect(dbfile)
@@ -14,28 +26,31 @@ class cntnt:
 	def revert(self):
 		self.conn.revert()
 
-	def read(self, id):
-		#TODO: add a flag named "followPointer" for pointer typed contents
-		try:
-			self.c.execute("select * from contents where id=%s"%id)
-			result = self.c.fetchone()
-			self.commit()
-		except:
-			result = False
-			self.revert()
-		#TODO: read must returns a dict
+	def read(self, id, followPointer = True, isPointed = False):
+		# TODO: add a flag named "followPointer" for pointer typed contents
+		result = {}
+		self.c.execute("select * from contents where id=%s"%id)
+		row = self.c.fetchone()
+		# Check if row exists
+		if not row:
+			raise self.ContentNotExixtsError
+		# If content is a pointer
+		if followPointer == True and row.type == 'ptr':
+			return self.read(row[content], isPointed = True)
+		# Prepare result dict
+		for key in row.keys():
+			result[key] = row[key]
+		# Add extra keys for pointers, etc.
+		result['isPointed'] = isPointed
 		return result
 
 	def readChilds(self, id):
-		try:
-			self.c.execute("select * from contents where parent=%s"% (id))
-		except:
-			return False
+		self.c.execute("select * from contents where parent=%s"% (id))
 		result = []
 		childs = self.c.fetchall()
 		for child in childs:
 			result.append(self.read(child.id))
-		return result or False
+		return result
 
 # Create table
 #c.execute('''CREATE TABLE "contents" (id INTEGER NOT NULL PRIMARY KEY UNIQUE, contentid INTEGER NOT NULL, content VARCHAR, label VARCHAR, type VARCHAR, parent INTEGER NOT NULL, startver INTEGER NOT NULL, endver INTEGER, createdate VARCHAR NOT NULL, deletedate VARCHAR);''')
@@ -44,23 +59,25 @@ class cntnt:
 # Insert root record
 # c.execute('''INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, createdate) VALUES (0 ,0 ,"root" ,"root" ,"content" ,0 , 0, "20070818100000" )''')
 	def create(self, content="", type="", parent=0, label=""):
-		#TODO: Check if type is defined
+		# TODO: Check if type is defined
 		# Check if label and type names are valid
 		def checkName(string):
 			return bool(re.match("^[A-Za-z0-9]*$",string))
-		if not checkName(label) or not checkName(type):
-			return False
+		if not checkName(type):
+			raise self.TypeNameError, 'Error in type name validation: "%s"' % type
+		if not checkName(label):
+			raise self.LabelNameError, 'Error in label name validation: "%s"' % label
 		# Check if parent exists
 		self.c.execute('select * from contents where id=%s' % parent)
 		if not self.c.fetchone():
-			return False
+			raise self.ParentNotFoundError
 		# Check if label is uniq
 		self.c.execute('select * from contents where parent=%s and label="%s"' % (parent, label))
 		if label != "" and self.c.fetchone():
-			return False
+			raise self.LabelNotUniqError, "Check label:%s" % label
 		# Get next content id
-		self.c.execute('select max(contentid) as ver from contents')
-		nextcid = int(1 + self.c.fetchone().ver)
+		self.c.execute('select max(contentid) as id from contents')
+		nextcid = int(1 + self.c.fetchone().id)
 		# Get next version number(id)
 		self.c.execute('select max(id) as ver from contents')
 		nextid = int(1 + self.c.fetchone().ver)
@@ -69,19 +86,18 @@ class cntnt:
 		sql = 'INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, createdate) VALUES (%s ,%s , "%s" ,"%s" ,"%s" ,%s , %s, "%s" )'
 		sql = sql % (nextid, nextcid, content, label, type, parent, nextid, createdate)
 		self.c.execute(sql)
+		self.commit()
 		id = self.c.lastrowid
 		return self.read(id)
 
 	def delete(self, id):
 		# Check if id exists
-		if not self.read(id):
-			return None
+		self.read(id)
 		# Check if content has childs
-		self.c.execute('select id from contents where parent = %s' % id)
-		childs = self.c.fetchall()
-		if len(childs)!=0:
-			return False
-		#TODO: delete() must UPDATE records (not DELETE) to be marked as deleted
+		childs = self.readChilds(id)
+		if len(childs) != 0:
+			raise ContentHasChilds, "Content has %s childs" % len(childs)
+		# TODO: delete() must UPDATE records (not DELETE) to be marked as deleted
 		# Delete record
 		sql = 'delete from contents where id = %s' % (id)
 		self.c.execute(sql)
@@ -89,37 +105,26 @@ class cntnt:
 		return id
 
 	def deepDelete(self, id):
-		self.c.execute('select id from contents where parent = %s' % (id))
-		childs = self.c.fetchall()
 		ids = []
-		for child in childs:
-			child_ids = self.deepDelete(child.id)
-			ids.extend(child_ids)
-		else:
-			id = self.delete(id)
-			if id:
-				ids.append(id)
-		if not ids:
-			result = False
-		else:
-			result = ids
+		for child in self.readChilds(id):
+			childIds = self.deepDelete(child["id"])
+			ids.extend(childIds)
+		self.delete(id)
+		ids.append(id)
 		# Return a list of deleted ids
-		return result
+		return ids
 
 # here after there is only command line functions
 def tree(cnt, id=0, level=0):
 	childs = cnt.readChilds(id)
-	if not childs:
-		return False
 	for child in childs:
-		print "%4d %s%s(%s)"%(child.id, " "*level*4, child.content, child.type)
-		tree(cnt, child.id, level+1)
+		print "%4d %s%s(%s)"%(child["id"], " "*level*4, child["content"], child["type"])
+		tree(cnt, child["id"], level+1)
 
 def view(cnt, id=None):
-	#TODO: Show all records - MASSIVE FUNCTION
-	all=[]
-	for i in all:
-		print "%4d %4d %s %s(%s)"%(i.id, i.parent, i.label, i.content, i.type)
+	# TODO: Show all records - MASSIVE FUNCTION
+	i=cnt.read(id)
+	print "%4d %4d %s %s(%s)"%(i["id"], i["parent"], i["label"], i["content"], i["type"])
 
 
 def usage():
@@ -128,7 +133,7 @@ def usage():
 
 def main():
 	import getopt
-	cnt = cntnt('/home/bekir/test/python/hede.db')
+	cnt = cntnt('hede.db')
 	try:
 		opts, args = getopt.getopt(sys.argv[1:], "ho:crudDt", ["help", "output=", "id=", "content=", "label=", "type=", "parent="])
 	except getopt.GetoptError:
@@ -175,7 +180,7 @@ def main():
 		if None in (content, type, parent):
 			print "You must supply --content, --type and --parent"
 			sys.exit(0)
-		return cnt.create(content, type, parent, label)
+		print cnt.create(content, type, parent, label)
 	elif crud == "read":
 		view(cnt, id)
 	elif crud == "tree":
@@ -184,9 +189,9 @@ def main():
 	elif crud == "update":
 		pass
 	elif crud == "delete":
-		return cnt.delete(id)
+		print cnt.delete(id)
 	elif crud == "deepdelete":
-		return cnt.deepDelete(id)
+		print cnt.deepDelete(id)
 	else:
 		usage()
 	sys.exit(0)
