5159a849403d94767683992238e19876a449b844
bekir <bekir@b67a7fcb-dc60-493a-b9c6-6ad9acd740d3>
2007-09-15 09:57:51 +0000

    * Corrected ContentNotExixtsError typo. * Updated SQL query string changes for string expressions * Added exception declleration strings * Added label and type parameters for readChilds() func. * Moved creation checks into another function(checkForCreate()), and   added update() function. * Added stub function getCPath() and its command line parameter   definitions. * Changed default parameter values for Command Line parameters.
    git-svn-id: https://svn.bdgn.net/public/test/python@17 b67a7fcb-dc60-493a-b9c6-6ad9acd740d3

--- cntnt.py
+++ cntnt.py
@@ -7,7 +7,8 @@ class cntnt:
 	# Exception Definitions
 	class LabelNameError(Exception): pass
 	class TypeNameError(Exception): pass
-	class ContentNotExixtsError(Exception): pass
+	class ContentExistsError(Exception): pass
+	class ContentNotExistsError(Exception): pass
 	class LabelNotUniqError(Exception): pass
 	class ContentHasChilds(Exception): pass
 	class ContentHasNoChilds(Exception): pass
@@ -24,11 +25,11 @@ class cntnt:
 		self.conn.revert()
 
 	def read(self, id, followPointer = True, isPointed = False):
-		self.c.execute('SELECT * FROM contents WHERE contentid = %s AND deletedate IS NULL'%id)
+		self.c.execute('SELECT * FROM contents WHERE contentid = "%s" AND deletedate IS NULL'%id)
 		row = self.c.fetchone()
 		# Check if row exists
 		if not row:
-			raise self.ContentNotExixtsError
+			raise self.ContentNotExistsError, 'ID not exists:"%s"' % id
 		# If content is a pointer
 		if followPointer == True and row.type == 'ptr':
 			return self.read(row[content], isPointed = True)
@@ -40,21 +41,20 @@ class cntnt:
 		result['isPointed'] = isPointed
 		return result
 
-	def readChilds(self, id):
-		self.c.execute('SELECT * FROM contents WHERE parent = %s AND deletedate IS NULL'% (id))
+	def readChilds(self, id, label = "", type = ""):
+		sqlexpr = ""
+		if label != "":
+			sqlexpr += 'AND label = "%s"' % label
+		if type != "":
+			sqlexpr += 'AND type = "%s"' % type
+		self.c.execute('SELECT * FROM contents WHERE parent = %s AND deletedate IS NULL %s' % (id, sqlexpr))
 		result = []
 		childs = self.c.fetchall()
 		for child in childs:
-			result.append(self.read(child.id))
+			result.append(self.read(child.contentid))
 		return result
 
-# Create table
-#c.execute('''CREATE TABLE "contents" (id INTEGER NOT NULL PRIMARY KEY UNIQUE, contentid INTEGER NOT NULL, content VARCHAR, label VARCHAR, type VARCHAR, parent INTEGER NOT NULL, startver INTEGER NOT NULL, endver INTEGER, createdate VARCHAR NOT NULL, deletedate VARCHAR);''')
-# Insert a row of data
-# c.execute('''INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, endver, createdate, deletedate) VALUES ( , , , , , , , , , )''')
-# Insert root record
-# c.execute('''INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, createdate) VALUES (0 ,0 ,"root" ,"root" ,"content" ,0 , 0, "20070818100000" )''')
-	def create(self, content="", type="", parent=0, label=""):
+	def checkForCreate(self, content="", type="", parent=0, label="", id=0):
 		# TODO: Check if type is defined
 		# Check if label and type names are valid
 		def checkName(string):
@@ -68,19 +68,35 @@ class cntnt:
 		self.c.execute('SELECT * FROM contents WHERE parent=%s AND label="%s" AND deletedate IS NULL' % (parent, label))
 		if label != "" and self.c.fetchone():
 			raise self.LabelNotUniqError, "Check label:%s" % label
-		# Get next content id
-		self.c.execute('SELECT MAX(contentid) AS contentid FROM contents')
-		nextcid = int(1 + self.c.fetchone().contentid)
-		# Get next version number(id)
+		# Check if content id exists
+		try:
+			if id and self.read(id):
+				raise self.ContentExistsError, "Content already exists which has this id:%s" % id
+		except self.ContentNotExistsError:
+			pass
+
+# Create table
+# c.execute('''CREATE TABLE "contents" (id INTEGER NOT NULL PRIMARY KEY UNIQUE, contentid INTEGER NOT NULL, content VARCHAR, label VARCHAR, type VARCHAR, parent INTEGER NOT NULL, startver INTEGER NOT NULL, endver INTEGER, createdate VARCHAR NOT NULL, deletedate VARCHAR);''')
+# Insert a row of data
+# c.execute('''INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, endver, createdate, deletedate) VALUES ( , , , , , , , , , )''')
+# Insert root record
+# c.execute('''INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, createdate) VALUES (0 ,0 ,"root" ,"root" ,"content" ,0 , 0, "20070818100000" )''')
+	def create(self, content="", type="", parent=0, label="", id=0):
+		# Do creation parameter checks, raises exception on parameter errors
+		self.checkForCreate(content=content, type=type, parent=parent, label=label, id=id)
+		# Get next version number(id) if its not given
+		if not id:
+			self.c.execute('SELECT MAX(contentid) AS contentid FROM contents')
+			id = int(1 + self.c.fetchone().contentid)
 		self.c.execute('SELECT MAX(id) AS ver FROM contents')
-		nextid = int(1 + self.c.fetchone().ver)
+		ver = int(1 + self.c.fetchone().ver)
 		# Create record
 		createdate = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-		sql = 'INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, createdate) VALUES (%s ,%s , "%s" ,"%s" ,"%s" ,%s , %s, "%s" )'
-		sql = sql % (nextid, nextcid, content, label, type, parent, nextid, createdate)
+		sql = 'INSERT INTO "contents" (id, contentid, content, label, type, parent, startver, createdate) VALUES ("%s" ,"%s" , "%s" ,"%s" ,"%s" ,"%s" , "%s", "%s" )'
+		sql = sql % (ver, id, content, label, type, parent, ver, createdate)
 		self.c.execute(sql)
 		self.commit()
-		return self.read(nextcid)
+		return self.read(id)
 
 	def delete(self, id):
 		# Check if content id exists
@@ -91,7 +107,7 @@ class cntnt:
 			raise ContentHasChilds, "Content has %s childs" % len(childs)
 		# Delete record
 		deletedate = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-		sql = 'UPDATE contents SET deletedate = "%s" WHERE contentid = %s AND deletedate IS NULL' % (deletedate, id)
+		sql = 'UPDATE contents SET deletedate = "%s" WHERE contentid = "%s" AND deletedate IS NULL' % (deletedate, id)
 		self.c.execute(sql)
 		self.commit()
 		return id
@@ -106,17 +122,54 @@ class cntnt:
 		# Return a list of deleted ids
 		return ids
 
+	def update(self, id, content="", type="", parent=0, label=""):
+		# Do creation parameter checks, raises exception on parameter errors
+		try:
+			self.checkForCreate(content=content, type=type, parent=parent, label=label, id=id)
+		except self.ContentExistsError:
+			pass
+		cnt = self.read(id)
+		self.delete(id)
+		if content: cnt["content"] = content
+		if type: cnt["type"] = type
+		if parent: cnt["parent"] = parent
+		if label: cnt["label"] = label
+		# FIXME: if new label exist or label/type names are not valid content will be deleted only
+		return self.create(content=content, type=type, parent=parent, label=label, id=id)
+
+	def getCPath(self, path, parent = 0):
+		# FIXME: Not implemented yet
+		branchExpr, residual = path.split(".",1)
+		arr = []
+		for id in self.executeBranchExpr(parent, branchExpr):
+			childIds = getCPath(residual, id)
+			arr.extend(childIds)
+		return arr
+
+	def executeBranchExpr(self, parent, expr):
+		# FIXME: Not implemented yet
+		# Type expression
+#		if expr[0,2] == "__":
+			# TODO: Not implenmented type expressions
+#			type = expr[2:]
+#			return []
+		#Label expression
+		if expr[0] == "_":
+			label = expr[1:]
+			return self.readChilds(parent, label = label)
+		return []
+
 # here after there is only command line functions
 def tree(cnt, id=0, level=0):
 	childs = cnt.readChilds(id)
 	for child in childs:
-		output = "%4d %s" % (child["id"], " "*level*4)
+		output = "%4d %s" % (child["contentid"], " "*level*4)
 		output += child["content"]
 		if child["label"]:
 			output += "[%s]" % child["label"]
 		output += "(%s)" % child["type"]
 		print output
-		tree(cnt, child["id"], level+1)
+		tree(cnt, child["contentid"], level+1)
 
 def usage():
 	print """Usage:
@@ -126,17 +179,18 @@ def main():
 	import getopt
 	cnt = cntnt('hede.db')
 	try:
-		opts, args = getopt.getopt(sys.argv[1:], "ho:crudDt", ["help", "output=", "id=", "content=", "label=", "type=", "parent="])
+		opts, args = getopt.getopt(sys.argv[1:], "ho:crudDt", ["help", "output=", "id=", "content=", "label=", "type=", "parent=", "path="])
 	except getopt.GetoptError:
 		usage()
 		sys.exit(2)
 
-	content = None
-	label = None
-	type = None
-	parent = None
+	content = ""
+	label = ""
+	type = ""
+	parent = 0
 	crud = None
-	id = None
+	id = 0
+	path = ""
 
 	for o, a in opts:
 		if o in ("-h", "--help"):
@@ -166,19 +220,23 @@ def main():
 			type = a
 		if o == "--parent":
 			parent = a
+		if o == "--path":
+			path = a
 
 	if crud == "create":
-		if None in (content, type, parent):
+		if "" in (content, type, parent):
 			print "You must supply --content, --type and --parent"
 			sys.exit(0)
-		print cnt.create(content, type, parent, label)
+		print cnt.create(content=content, type=type, parent=parent, label=label, id=id)
 	elif crud == "read":
-		print cnt.read(id)
+		if path:
+			print cnt.getCPath(path)
+		if id:
+			print cnt.read(id)
 	elif crud == "tree":
-		if id == None: id=0
 		tree(cnt, id)
 	elif crud == "update":
-		pass
+		print cnt.update(id=id, content=content, type=type, parent=parent, label=label)
 	elif crud == "delete":
 		print cnt.delete(id)
 	elif crud == "deepdelete":
