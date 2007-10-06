defc947cd9cb9583b3854e0ccbfe5fefce93f966
bekir <bekir@b67a7fcb-dc60-493a-b9c6-6ad9acd740d3>
2007-10-06 13:36:10 +0000

    Removed some unnecessary comments
    git-svn-id: https://svn.bdgn.net/public/test/python@40 b67a7fcb-dc60-493a-b9c6-6ad9acd740d3

--- parser.py
+++ parser.py
@@ -39,48 +39,14 @@ def t_error(t):
 	print "Illegal char '%s'" % t.value
 	raise ParseError, "CPath is not valid!"
 
-def test(data):
+def lextest(data):
 	lex.input(data)
 	while 1:
 		tok = lex.token()
 		if not tok: break
 		print tok
 
-# parser bnf
-
-"""
-
-_basic._views.__view(_name.__type=query and _type=1).*.__type
-
-TOKENS:
--------
-DOT, LABELPREFIX, TYPEPREFIX, WORD, STAR, LPAREN, RPAREN, EQUAL, AND, OR
-
-PARSER BNF:
------------
-type : LABELPREFIX WORD
-
-label : TYPEPREFIX WORD
-
-name : type
-	 | label
-
-equation : name EQUAL WORD
-		 | cpath equal WORD
-
-inparen : inparen AND cpath
-		| inparen OR cpath
-
-paren : LPAREN inparen RPAREN
-
-branchexpr : name
-		   | STAR
-		   | name paren
-		   | STAR paren
-
-cpath : branchexpr DOT cpath | branchexpr
-"""
-
+# parser
 def p_cpath_dot(p):
 	'''cpath : cpath DOT branchexpr'''
 	p[0] = p[1] + "." + p[3]
@@ -144,12 +110,13 @@ def p_error(p):
 	print "Syntax error in input!"
 
 #testCPath = "test.hede"
-testCPath = "_test.__de(__deneme._h=op and _hoppa.heb=lek).hede"
+#testCPath = "_test.__de(__deneme._h=op and _hoppa.heb=lek).hede"
+testCPath = "_basic._views.__view(_name.__type=query and _type=1).*.__type"
 #testCPath = "_basic._views.__view(_name=hede and _type=1).*.__type"
 
 # lexical analyzer
 lex.lex(debug=0)
-#test(testCPath)
+#lextest(testCPath)
 
 # Build the parser
 yacc.yacc(debug=1)
@@ -157,4 +124,7 @@ yacc.yacc(debug=1)
 # Use this if you want to build the parser using SLR instead of LALR
 #yacc.yacc(method="SLR")
 
-print yacc.parse(testCPath, debug=1)
+try:
+	print yacc.parse(testCPath, debug=1)
+except:
+	raise ParseError, "CPath does not look valid!"
